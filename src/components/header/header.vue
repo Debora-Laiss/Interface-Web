763188c3511650fe52d7b7a1b7a10626fe34b38e
Debora Lais <deboralaisnm@gmail.com>
2025-03-30 20:13:08 -0300

    feat(header): add logo and navigation links to header

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -1,25 +1,32 @@
 <template>
-    <div id="header">
-      <router-link id="logo-url" to="/">
-        <img id="logo" :src="logo" :alt="alt">
-      </router-link>
-      <router-link to="/">Home</router-link>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    name: "Header",
-    props: ["logo", "alt"]
+  <div id="header">
+    <router-link id="logo-url" to="/">
+      <img id="logo" :src="logo" :alt="alt">
+    </router-link>
+    <router-link to="/">Home</router-link>
+  </div>
+</template>
+
+<script>
+import logoImg from "../../assets/logo.png"; 
+export default {
+  name: "Header",
+  data() {
+    return {
+      logo: logoImg,
+      alt: "Logo da empresa"
+    };
   }
-  </script>
+};
+</script>
   <style scoped>
   #header {
     display: flex;
     align-items: center;
     justify-content: space-between;
     padding: 1rem;
-    background-color: #bad6f1;
+    background-color: #222;
+    
     box-shadow: 0 2px 4px rgba(0,0,0,0.1);
     border-radius: 0.3rem;
   }
@@ -32,10 +39,11 @@
     height: 50px;
     max-width: 150px;
     object-fit: contain;
+    border-radius: 4px;
   }
   
   #logo-url + a {
-    color: #333;
+    color: #ffffff;
     text-decoration: none;
     font-weight: 600;
   }
